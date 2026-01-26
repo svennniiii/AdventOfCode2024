471424873c7030d58f731a72602acdc2d5f121bf
Sven Bodenstedt <19672380+svennniiii@users.noreply.github.com>
2026-01-26 16:16:11 +0100

    Day 14 Part 2

--- src/bin/2024-14.rs
+++ src/bin/2024-14.rs
@@ -1,6 +1,9 @@
 use std::fs;
 use std::time::Instant;
 
+use std::thread;
+use std::time::Duration;
+
 struct Position {
     x: i32,
     y: i32
@@ -16,6 +19,26 @@ impl Robot {
         self.pos.x = (self.pos.x + self.vel.x).rem_euclid(width);
         self.pos.y = (self.pos.y + self.vel.y).rem_euclid(height);
     }
+}
+
+fn print_robots(height: usize, width: usize, robots: &Vec<Robot>) {
+    let mut map: Vec<Vec<bool>> = vec![vec![false; width]; height];
+
+    for robot in robots {
+        let Position{x, y} = robot.pos;
+        map[y as usize][x as usize] = true;
+    }
+
+    for row in map.iter() {
+        for c in row {
+            if *c == true {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    } 
 }
 
 fn main() {
@@ -51,7 +74,7 @@ fn main() {
     for _ in 0..100 {
         for robot in robots.iter_mut() {
             robot.move_step(height, width);
-        }
+        }        
     }
 
     let (mut q1, mut q2, mut q3, mut q4) = (0, 0, 0, 0);
@@ -76,6 +99,45 @@ fn main() {
 
     let safety_factor = q1 * q2 * q3 * q4;
 
+    let mut min_safety_factor = 224969976;
+    
+    for i in 0..100000 {
+         for robot in robots.iter_mut() {
+            robot.move_step(height, width);
+        }
+
+        q1 = 0;
+        q2 = 0;
+        q3 = 0;
+        q4 = 0;
+
+        for robot in robots.iter() {
+            let x = robot.pos.x;
+            let y = robot.pos.y;
+
+            // Robots exactly on the middle lines are ignored per rules
+            if x < mid_x && y < mid_y {
+                q1 += 1;
+            } else if x > mid_x && y < mid_y {
+                q2 += 1;
+            } else if x < mid_x && y > mid_y {
+                q3 += 1;
+            } else if x > mid_x && y > mid_y {
+                q4 += 1;
+            }
+        }
+
+        let safety_factor = q1 * q2 * q3 * q4;
+
+        if safety_factor < min_safety_factor {
+            println!("{}:", i+100+1);
+            print_robots(height as usize, width as usize, &robots);
+            thread::sleep(Duration::from_millis(1000));  // Sleep for 500 milliseconds
+            min_safety_factor = safety_factor;
+        }
+    }
+
     println!("Part 1 Answer: {}", safety_factor);
+    println!("Part 2 Answer: {}", safety_factor);
     println!("Time: {:?}", start_time.elapsed());
 }
